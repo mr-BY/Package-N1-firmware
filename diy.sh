1f4420cf017f2ab340e60c551ab95987fa3ec64e
mrBY <34572441+mr-BY@users.noreply.github.com>
2021-10-14 12:40:36 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
-sed -i "s/WHOAMI\=\"活雷锋\"/WHOAMI\=\"mrBY\"/g" /opt/openwrt_packit/make.env
+sed -i "s/\(.*WHOAMI=\"\).*\(\".*\)/\1mrBY\2/g" /opt/openwrt_packit/make.env
 
-sed -i "s/^KERNEL_VERSION..\+\"/#&/g" /opt/openwrt_packit/make.env
-sed -i "s/^#\(KERNEL_VERSION..\+o\"\)/\1/g" /opt/openwrt_packit/make.env
+sed -i "s/\(.*\)\(KERNEL_VERSION=\".*+\".*\)/\1#\2/g" /opt/openwrt_packit/make.env
+sed -i "s/\(.*\)#\(KERNEL_VERSION=\".*+o\".*\)/\1\2/g" /opt/openwrt_packit/make.env
